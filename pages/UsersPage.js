ad0132960df3768afb98295ce293e87e40b3a3f7
melissapennington <mlpennington9@gmail.com>
2023-11-11 13:52:23 -0600

    users showing

--- pages/UsersPage.js
+++ pages/UsersPage.js
@@ -1,29 +1,33 @@
+// Profile.js
+
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { getSingleUser } from '../api/userData';
+import { getAllUsers } from '../api/userData';
 
 function Profile({ token }) {
-  const [profile, setProfile] = useState({});
+  const [users, setUsers] = useState([]);
 
-  const getTheSingleUser = () => {
-    getSingleUser(token).then(setProfile);
+  const getTheUsers = () => {
+    getAllUsers(token).then(setUsers);
   };
 
   useEffect(() => {
-    getTheSingleUser(token);
-  }, []);
+    getTheUsers(token);
+  }, [token]);
 
   return (
     <div className="profile">
-      <div>
-        <img src={profile?.profile_image_url} alt={profile?.first_name} style={{ width: '100px' }} />
-        <h3>Name: {profile?.first_name} {profile?.last_name}</h3>
-        <br />
-        <h3>Email: {profile?.email}</h3>
-        <h3>Bio: {profile?.bio}</h3>
-        <h3>Username: {profile?.username}</h3>
-        <h3>Active Since: {profile?.created_on}</h3>
-      </div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <img src={user.profile_image_url} alt={user.first_name} style={{ width: '100px' }} />
+          <h3>Name: {user.first_name} {user.last_name}</h3>
+          <br />
+          <h3>Email: {user.email}</h3>
+          <h3>Bio: {user.bio}</h3>
+          <h3>Username: {user.username}</h3>
+          <h3>Active Since: {user.created_on}</h3>
+        </div>
+      ))}
     </div>
   );
 }
