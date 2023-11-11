ab33d76f6b68ae60ce6a02f3b981d5fbe2eb316a
datcog1111 <h9tuck@gmail.com>
2023-11-11 10:12:50 -0600

    for pull

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,31 @@
-// import React, { useEffect, useState } from 'react';
-// import Head from 'next/head';
-// import PostCard from '../components/postCard';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import PostCard from '../components/postCard';
+import getAllPosts from '../api/postData';
 
-// export default function Home() {
-//   const [posts, setPosts] = useState([]);
+export default function Home() {
+  const [posts, setPosts] = useState([]);
 
-//   const getAllThePosts = () => {
-//     getAllPosts().then(setPosts);
-//   };
+  const getAllThePosts = () => {
+    getAllPosts().then(setPosts);
+  };
 
-//   useEffect(() => {
-//     getAllThePosts();
-//   }, []);
+  useEffect(() => {
+    getAllThePosts();
+  }, []);
 
-//   return (
-//     <>
-//       <Head>
-//         <title>Homepage</title>
-//       </Head>
-//       <div className="text-center my-4">
-//         <div id="home-cards" className="d-flex flex-wrap">
-//           {posts.map((post) => (
-//             <PostCard key={post.id} postObj={post} onUpdate={getAllThePosts} />
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <div className="text-center my-4">
+        <div id="home-cards" className="d-flex flex-wrap">
+          {posts.map((post) => (
+            <PostCard key={post.id} postObj={post} onUpdate={getAllThePosts} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
