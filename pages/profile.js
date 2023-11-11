47376deb005135e3c968a30434f951f504154e82
caby-erin <caby27@gmail.com>
2023-11-11 12:32:27 -0600

    profile page not working

--- pages/profile.js
+++ pages/profile.js
@@ -1,29 +1,30 @@
-/*
 import React, { useEffect, useState } from 'react';
+import { getSingleUser } from '../api/userData';
 
 function Profile() {
-  const [profile, setProfile] = useState([]);
+  const [profile, setProfile] = useState({});
+
+  const getTheSingleUser = () => {
+    getSingleUser(profile.id).then(setProfile);
+  };
 
   useEffect(() => {
-    getSingleUser().then(setProfile);
+    getTheSingleUser(profile.id);
   }, []);
 
   return (
     <div className="profile">
-      {profile.map((user) => (
-        <div>
-          <img src={user.profile_image_url} alt={user.first_name} style={{ width: '100px' }} />
-          <h3>Name: {user.first_name} {user.last_name}</h3>
-          <br />
-          <h3>Email: {user.email}</h3>
-          <h3>Bio: {user.bio}</h3>
-          <h3>Username: {user.username}</h3>
-          <h3>Active Since: {user.created_on}</h3>
-        </div>
-      ))}
+      <div>
+        <img src={profile?.profile_image_url} alt={profile?.first_name} style={{ width: '100px' }} />
+        <h3>Name: {profile?.first_name} {profile?.last_name}</h3>
+        <br />
+        <h3>Email: {profile?.email}</h3>
+        <h3>Bio: {profile?.bio}</h3>
+        <h3>Username: {profile?.username}</h3>
+        <h3>Active Since: {profile?.created_on}</h3>
+      </div>
     </div>
   );
 }
 
 export default Profile;
-*/
